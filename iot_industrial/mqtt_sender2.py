7e00794c34c9dbdb95c34a331b6bdde49015aab7
Lao <lao.abarca@unmsm.edu.pe>
2026-07-14 17:56:28 -0500

    Update sensor simulation description for zone 2

--- iot_industrial/mqtt_sender2.py
+++ iot_industrial/mqtt_sender2.py
@@ -2,8 +2,8 @@ import paho.mqtt.client as mqtt
 import json
 import time
 import random
-# Simulación de un sensor de la zona 1, 
-# publicando datos cada 10 segundos en el tópico "fisi/ecopulse/zona/1"
+# Simulación de un sensor de la zona 2, 
+# publicando datos cada 10 segundos en el tópico "fisi/ecopulse/zona/2"
 BROKER = "broker.hivemq.com" # Broker público para pruebas
 PORT = 1883
 ID_ZONA = 2
